052f3943c09ce6a99c88ec73409b13069eb47b8f
Allan <allan.kimurgor@student.moringaschool.com>
2022-08-22 23:53:40 +0300

    Completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  let form = document.querySelector('form');
+  form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    handleTodo(e.target.new_task.value);
+    form.reset();
+  })
 });
